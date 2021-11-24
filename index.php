2198b243fe6e903f95e55891002d98c15fb11e9d
NTX8205 <ad0124578963@gmail.com>
2021-11-24 23:40:34 +0800

    clear

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>個人頁面</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
+    <div class="container">
+      <a class="navbar-brand" href="#">索引</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample07">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../dashboard/index.html">xampp 介紹</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/phpmyadmin/">phpmyadmin</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="info.php">phpinfo</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">其他頁面</a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown07">
+              <li>
+                <a class="dropdown-item" href="./board/ui/index.php">留言板</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./board/example/pdo.php">測試(pdo連線)</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./board/example/example.html">測試2(留言板)</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./board/example/upimage.html">測試3(圖)</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./board/example/getimage.php">圖</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./board/ui/login.php">登入</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./menu/interface/menu.php">菜單</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./extest/index.html">人數</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./test/index.php">個人網頁</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+</body>
+
+
+</html>
