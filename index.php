b38bb401f749af103d8a2da0d9cc0df8c35c6364
NTX0325 <ad0124578963@gmail.com>
2022-03-05 16:21:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 <a class="dropdown-item" href="./board/ui/index.php">留言板</a>
               </li>
               <li>
-                <a class="dropdown-item" href="./board_v2/board.php">留言板V2</a>
+                <a class="dropdown-item" href="./board_v2/board/board.php">留言板V2</a>
               </li>
               <li>
                 <a class="dropdown-item" href="./board/example/pdo.php">測試(pdo連線)</a>
