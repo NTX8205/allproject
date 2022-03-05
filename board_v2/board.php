2c9a4cbf01ca3387123d2cb604aafc880c0153eb
NTX0325 <ad0124578963@gmail.com>
2022-03-05 16:06:53 +0800

    Update board.php

--- board_v2/board.php
+++ board_v2/board.php
@@ -61,58 +61,58 @@ if ($result = mysqli_query($connect, $sql)) {
         ?>
     </div>
 
-        <div class="modal fade" id="newmes" tabindex="-1" aria-labelledby="message" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="message">新增留言</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form action="/allproject/board_v2/function.php" method="post">
-                            <div class="mb-3">
-                                <input type="text" class="form-control" id="username" name="user" placeholder="名稱">
-                            </div>
-                            <div class="mb-3">
-                                <textarea class="form-control" id="content" name="messages" rows="3" placeholder="輸入內容"></textarea>
-                            </div>
+    <div class="modal fade" id="newmes" tabindex="-1" aria-labelledby="message" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="message">新增留言</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="/allproject/board_v2/function.php" method="post">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" id="username" name="user" placeholder="名稱">
+                        </div>
+                        <div class="mb-3">
+                            <textarea class="form-control" id="content" name="messages" rows="3" placeholder="輸入內容"></textarea>
+                        </div>
 
-                            <div class="mb-3">
-                                <input type="button" class="btn btn-outline-danger" data-bs-dismiss="modal" value="關閉" id="close"></input>
-                                <button type="submit" class="btn btn-outline-success" name="method" value="add">新增</input>
-                            </div>
-                        </form>
-                    </div>
+                        <div class="mb-3">
+                            <input type="button" class="btn btn-outline-danger" data-bs-dismiss="modal" value="關閉" id="close"></input>
+                            <button type="submit" class="btn btn-outline-success" name="method" value="add">新增</input>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="modal fade" id="editmes" tabindex="-1" aria-labelledby="message" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="message">編輯留言</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form action="/allproject/board_v2/function.php" method="post">
-                            <div class="mb-3">
-                                <input type="text" class="form-control" id="edtusername" name="user" value="">
-                            </div>
-                            <div class="mb-3">
-                                <textarea class="form-control" id="edtcontent" name="messages" rows="3"></textarea>
-                            </div>
+    <div class="modal fade" id="editmes" tabindex="-1" aria-labelledby="message" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="message">編輯留言</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="/allproject/board_v2/function.php" method="post">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" id="edtusername" name="user" value="">
+                        </div>
+                        <div class="mb-3">
+                            <textarea class="form-control" id="edtcontent" name="messages" rows="3"></textarea>
+                        </div>
 
-                            <div class="mb-3">
-                                <input type="button" class="btn btn-outline-danger" data-bs-dismiss="modal" value="關閉" id="close">
-                                <button type="submit" class="btn btn-outline-success" name="method" value="edit">編輯</button>
-                                <input type="hidden" id="edtidm" name="id" value="">
-                            </div>
-                        </form>
-                    </div>
+                        <div class="mb-3">
+                            <input type="button" class="btn btn-outline-danger" data-bs-dismiss="modal" value="關閉" id="close">
+                            <button type="submit" class="btn btn-outline-success" name="method" value="edit">編輯</button>
+                            <input type="hidden" id="edtidm" name="id" value="">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 </body>
 
 </html>
